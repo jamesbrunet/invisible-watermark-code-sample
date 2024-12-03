da63c633a6f0bc5d20ed6ef6e9692345e19a257f
James Brunet <james@brunet.dev>
2024-12-03 12:50:45 -0500

    fix extension

--- find-watermark.py
+++ find-watermark.py
@@ -1,7 +1,7 @@
 import cv2
 from imwatermark import WatermarkDecoder
 
-watermarked_image = cv2.imread('james-watermarked.png')
+watermarked_image = cv2.imread('james-watermarked.jpg')
 
 decoder = WatermarkDecoder('bytes', 64)
 watermark = decoder.decode(watermarked_image, 'dwtDctSvd')
